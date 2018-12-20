64f7427588e00e779852acbc01c6b9f9cbe0daef
Christoph Hösler <christoph.hoesler@gmail.com>
2018-12-21 00:40:07 +0100

    fix(indexer): don't flush closed stream (#6748)

--- indexing-hadoop/src/main/java/io/druid/indexer/JobHelper.java
+++ indexing-hadoop/src/main/java/io/druid/indexer/JobHelper.java
@@ -504,7 +504,6 @@ public class JobHelper
                   progressable
               )) {
                 HadoopDruidIndexerConfig.JSON_MAPPER.writeValue(descriptorOut, segment);
-                descriptorOut.flush();
               }
             }
             catch (RuntimeException | IOException ex) {
